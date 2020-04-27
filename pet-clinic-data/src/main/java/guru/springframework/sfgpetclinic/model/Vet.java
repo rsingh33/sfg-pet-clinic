095629e8a7ee8308d32678f1451dcc09d833ab6b
Rahul <rsingh33@buffalo.edu>
2020-04-27 18:34:17 -0400

    Create Spring Data Jpa Service for owner. closes #40.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -11,8 +11,7 @@ public class Vet extends Person{
     @ManyToMany(fetch = FetchType.EAGER )
     @JoinTable(name = "vet_specialities",
             joinColumns = @JoinColumn(name= "vet_id"),
-            inverseJoinColumns = @JoinColumn(name= "vet_id"))
-
+            inverseJoinColumns = @JoinColumn(name= "speciality_id"))
             private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
