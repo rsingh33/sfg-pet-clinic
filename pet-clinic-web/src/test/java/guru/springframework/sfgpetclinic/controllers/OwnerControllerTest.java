9833ad841e195b0a506188a6396a7e4b8c3e69c5
Rahul <rsingh33@buffalo.edu>
2020-05-01 12:10:37 -0400

    Fix Display Owner. closes #62

--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -14,8 +14,9 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.hamcrest.Matchers.hasSize;
-import static org.mockito.Mockito.when;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -50,13 +51,27 @@ class OwnerControllerTest {
         mockMvc.perform(get("/owners"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("owner/owner-index"))
-                .andExpect(model().attribute("owners",hasSize(2)));
+                .andExpect(model().attribute("owners", hasSize(2)));
     }
 
     @Test
     void findOwners() throws Exception {
         mockMvc.perform(get("/owners/find"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("owner/notimplemented"));
+                .andExpect(view().name("/notimplemented"));
+    }
+
+    @Test
+    void showOwner() throws Exception {
+        Owner owner = Owner.builder().id(2L).firstName("Rahul").build();
+        when(ownerService.findById(anyLong())).thenReturn(owner);
+
+        mockMvc.perform(get("/owners/2"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owner/ownerDetails"))
+                .andExpect(model().attributeExists("owner"))
+                .andExpect(model().attribute("owner", hasProperty("id",is(2L))));
+
+        verify(ownerService, times(1)).findById(anyLong());
     }
 }
